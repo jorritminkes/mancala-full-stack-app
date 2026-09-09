e9c8592774cbab9a30596bb6dddb133b14301041
jorrit <jorritminkes@gmail.com>
2026-09-09 14:21:29 +0200

    Added button for playing a pocket

--- client/src/components/MancalaBoard.tsx
+++ client/src/components/MancalaBoard.tsx
@@ -1,11 +1,10 @@
+import { useState } from 'react';
+
 export function MancalaBoard(props: any) {
   
   // const { bord, klikOpPocket } = props;
   // const stenen = bord.stenenPerVakje;
-  const stenen = [4,4,4,4,4,4, 0, 4,4,4,4,4,4, 0]
-
-  const vakjesSpeler1 = [4, 4, 4, 4, 4, 4, 0];
-  const vakjesSpeler2 = [4, 4, 4, 4, 4, 4, 0];
+  const [stenen, setStenen] = useState([4,4,4,4,4,4, 0, 4,4,4,4,4,4, 0]);
 
   const outerPadding = 20;
   const gap = 20;
@@ -40,7 +39,22 @@ export function MancalaBoard(props: any) {
     // fontWeight: 'bold',
     fontSize: '22px',
     // boxShadow: 'inset 0 2px 5px rgba(0,0,0,0.4)'
-  }
+  }async function
+
+  async function playMove(pocketPositie: number) {
+      console.log("Clicked pocket")
+      try {
+          const response = await fetch(`http://localhost:8080/api/game/move/${pocketPositie}`, {
+              method: "POST"
+          });
+          const result = await response.json();
+          console.log("Board updated:", result);
+          setStenen(result.stenenPerVakje);
+      } catch (error) {
+          console.error("Error:", error);
+          alert("Invalid move!")
+      }
+    }
 
   
 
@@ -60,66 +74,29 @@ export function MancalaBoard(props: any) {
       }}>
         
         <div style={mancalaStyle}>
-          {/* {vakjesSpeler2[6]} */}
           {stenen[13]}
         </div>
         
         <div style={{ display: 'flex', flexDirection: 'column', gap: `${gap}px` }}>
           <div style={{ display: 'flex', gap: `${gap}px` }}>
-            <div style={pocketStyle} onClick={() => playMove(1)}>{vakjesSpeler2[5]}</div>
-            <div style={pocketStyle}>{vakjesSpeler2[4]}</div>
-            <div style={pocketStyle}>{vakjesSpeler2[3]}</div>
-            <div style={pocketStyle}>{vakjesSpeler2[2]}</div>
-            <div style={pocketStyle}>{vakjesSpeler2[1]}</div>
-            <div style={pocketStyle}>{vakjesSpeler2[0]}</div>
+            <div style={pocketStyle} onClick={() => playMove(13)}>{stenen[12]}</div>
+            <div style={pocketStyle} onClick={() => playMove(12)}>{stenen[11]}</div>
+            <div style={pocketStyle} onClick={() => playMove(11)}>{stenen[10]}</div>
+            <div style={pocketStyle} onClick={() => playMove(10)}>{stenen[9]}</div>
+            <div style={pocketStyle} onClick={() => playMove(9)}>{stenen[8]}</div>
+            <div style={pocketStyle} onClick={() => playMove(8)}>{stenen[7]}</div>
           </div>
           <div style={{ display: 'flex', gap: `${gap}px` }}>
-            <div style={pocketStyle}>{vakjesSpeler1[0]}</div>
-            <div style={pocketStyle}>{vakjesSpeler1[1]}</div>
-            <div style={pocketStyle}>{vakjesSpeler1[2]}</div>
-            <div style={pocketStyle}>{vakjesSpeler1[3]}</div>
-            <div style={pocketStyle}>{vakjesSpeler1[4]}</div>
-            <div style={pocketStyle}>{vakjesSpeler1[5]}</div>
+            <div style={pocketStyle} onClick={() => playMove(1)}>{stenen[0]}</div>
+            <div style={pocketStyle} onClick={() => playMove(2)}>{stenen[1]}</div>
+            <div style={pocketStyle} onClick={() => playMove(3)}>{stenen[2]}</div>
+            <div style={pocketStyle} onClick={() => playMove(4)}>{stenen[3]}</div>
+            <div style={pocketStyle} onClick={() => playMove(5)}>{stenen[4]}</div>
+            <div style={pocketStyle} onClick={() => playMove(6)}>{stenen[5]}</div>
           </div>
-
-          
-          {/* <div style={{ display: 'flex', gap: `${gap}px` }}>
-            <div style={pocketStyle}onClick={() => klikOpPocket(13)}>{vakjesSpeler2[5]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(12)}>{vakjesSpeler2[4]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(11)}>{vakjesSpeler2[3]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(10)}>{vakjesSpeler2[2]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(9)}>{vakjesSpeler2[1]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(8)}>{vakjesSpeler2[0]}</div>
-          </div>
-          <div style={{ display: 'flex', gap: `${gap}px` }}>
-            <div style={pocketStyle}onClick={() => klikOpPocket(0)}>{vakjesSpeler1[0]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(1)}>{vakjesSpeler1[1]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(2)}>{vakjesSpeler1[2]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(3)}>{vakjesSpeler1[3]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(4)}>{vakjesSpeler1[4]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(5)}>{vakjesSpeler1[5]}</div>
-          </div> */}
-          
-          {/* <div style={{ display: 'flex', gap: `${gap}px` }}>
-            <div style={pocketStyle}onClick={() => klikOpPocket(13)}>{stenen[12]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(12)}>{stenen[11]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(11)}>{stenen[10]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(10)}>{stenen[9]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(9)}>{stenen[8]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(8)}>{stenen[7]}</div>
-          </div>
-          <div style={{ display: 'flex', gap: `${gap}px` }}>
-            <div style={pocketStyle}onClick={() => klikOpPocket(0)}>{stenen[0]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(1)}>{stenen[1]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(2)}>{stenen[2]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(3)}>{stenen[3]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(4)}>{stenen[4]}</div>
-            <div style={pocketStyle}onClick={() => klikOpPocket(5)}>{stenen[5]}</div>
-          </div> */}
         </div>
         
         <div style={mancalaStyle}>
-          {/* {vakjesSpeler1[6]} */}
           {stenen[6]}
         </div>
       </div>      
