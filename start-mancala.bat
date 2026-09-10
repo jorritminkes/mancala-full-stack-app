64fcece83e2e69b220cf579c9080650c647fbc2c
jorrit <jorritminkes@gmail.com>
2026-09-10 14:50:08 +0200

    removed cluttering files from git repo root folder

--- start-mancala.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-wt -w 0 new-tab --title "Mancala Client" cmd /k "cd /d C:\Users\jorri\Documents\git-repos\mancala-app\client && npm run dev" ; new-tab --title "Mancala API" cmd /k "cd /d C:\Users\jorri\Documents\git-repos\mancala-app\api && mvn spring-boot:run"
